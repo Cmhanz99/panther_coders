4bf2068dc455d7de72c7f1223beb4e125c504528
Cmhanz99 <hanzhmagbal@gmail.com>
2025-04-14 16:20:43 +0800

    navbarlayout

---
+++ nav.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <h1>Property<span>Finder</span></h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#home" class="active">Home</a></li>
+                <li><a href="#properties">Properties</a></li>
+                <li><a href="#map">Map View</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="/sign_up" class="btn-primary">Sign In</a></li>
+            </ul>
+        </nav>
+        <div class="mobile-menu-toggle">
+            <i class="fas fa-bars"></i>
+        </div>
+    </div>
+</header>
